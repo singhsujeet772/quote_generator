514970ffd0c66a89e5d35b7500ddb34a2ab7973f
Sujeet <singhsujeet772@gmail.com>
2024-01-07 11:59:13 +0530

    add loader text show in div button click eventlisteners

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+//Show Loading
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;   
+}
 
 //get random quote using locallly save json file
 
@@ -19,12 +33,33 @@ let apiQuotes = [];
 //get random quaote using api
 
 function newQuote(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    //check that author is not null
+
+    if(!quote.author){
+        quoteAuthor.textContent = 'Author';
+    }else{
+        quoteAuthor.textContent = quote.author;
+    }
+    
+    //check if quote is lengthy than change style
+
+    if(quote.text.length > 50){
+        quoteText.classList.add('long-text');
+    }else{
+        quoteText.classList.remove('long-text');
+    }
+
+    //show quote and hide loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 //Get Quotes From Api
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -35,4 +70,15 @@ async function getQuotes(){
     }
 }
 
+//Tweet Quote
+
+function tweetQuote(){
+    const twitterUrl = `http://twitter.com/intent/tweet?$text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+//Event Listeners
+newQuoteBtn.addEventListener("click",newQuote);
+twitterBtn.addEventListener("click",tweetQuote);
+
+//On load
 getQuotes();
